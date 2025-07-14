8638832633d10a2ad70ccb68ccde9a436095f3cf
lshh <markhong93@naver.com>
2025-07-15 04:12:49 +0900

    feat: user 회원가입, 조회 API 구현

---
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1ApiSpec.java
@@ -0,0 +1,26 @@
+package com.loopers.interfaces.api.user;
+
+import com.loopers.interfaces.api.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Users V1 API", description = "회원가입, 내 정보 조회")
+public interface UserV1ApiSpec {
+    @Operation(
+            summary = "회원가입",
+            description = "회원가입"
+    )
+    ApiResponse<UserV1Dto.UsersResponse> signUp(
+            @Schema(name = "요청", description = "회원가입할 사용자 정보 입력")
+            UserV1Dto.UsersRegisterRequest request
+    );
+
+    @Operation(
+            summary = "내 정보 조회",
+            description = "내 정보 조회"
+    )
+    ApiResponse<UserV1Dto.UsersResponse> getMyInfo(
+            @Schema(name = "조회할 아이디", description = "X-USER-ID") String loginId
+    );
+}
---
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1Dto.java
@@ -0,0 +1,29 @@
+package com.loopers.interfaces.api.user;
+
+import com.loopers.application.user.UserInfo;
+import com.loopers.domain.user.UserCommand;
+
+public class UserV1Dto {
+    public record UsersResponse(
+            Long id, String loginId, String gender, String birthDate, String email
+    ) {
+        public static UsersResponse from(UserInfo info) {
+            return new UsersResponse(
+                info.id(),
+                info.loginId(),
+                info.gender(),
+                info.birthDate(),
+                info.email()
+            );
+        }
+    }
+
+
+    public record UsersRegisterRequest(
+            String loginId, String gender, String birthDate, String email
+    ) {
+        public UserCommand.Create toCommand() {
+            return UserCommand.Create.of(loginId, gender, birthDate, email);
+        }
+    }
+}
